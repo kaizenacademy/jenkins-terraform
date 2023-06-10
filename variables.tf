73e95fdec039e1eaa0408a287224228716a40534
kaizenacademy <kaizenacademy@gmail.com>
2023-06-10 17:55:42 +0000

    added varibales file

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable ami_id {
+  type        = string
+  default     = "ami-092b51d9008adea15"
+  description = "description"
+}
+
+variable instance_type {
+  type        = string
+  default     = "t2.micro"
+  description = "description"
+}
+
+
+variable az1 {
+  type        = string
+  default     = "us-east-2a"
+  description = "description"
+}
+
+
+variable key_pair {
+  type        = string
+  default     = "my-laptop-key"
+  description = "description"
+}
+
