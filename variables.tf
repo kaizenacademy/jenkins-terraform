1d123fc312c7ec5270310df97d1bdf13c6e1b09d
kaizenacademy <126742188+kaizenacademy@users.noreply.github.com>
2023-06-15 19:44:11 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,3 +30,11 @@ variable region {
   description = "description"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+  owners = ["self"]
+}
